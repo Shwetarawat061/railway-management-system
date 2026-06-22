ae15b867a51ecec91117c901c8d5e95a9bd82e0f
Shweta Dayal Singh Rawat <shwetabts306@gmail.com>
2026-06-22 18:08:05 +0530

    Update train_routes.py

--- backend/routes/train_routes.py
+++ backend/routes/train_routes.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, jsonify
+from db import get_connection
+from flask_jwt_extended import jwt_required
 
+train_bp = Blueprint("train", __name__)
+
+@train_bp.route("/", methods=["GET"])
+@jwt_required()
+def get_trains():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM train_info")
+    return jsonify(cursor.fetchall())
