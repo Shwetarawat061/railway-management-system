f66041b3394c2d2b9d214f2ea9094d8b70988865
Shweta Dayal Singh Rawat <shwetabts306@gmail.com>
2026-06-22 17:56:04 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,21 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from config import Config
 
+from routes.auth_routes import auth_bp
+from routes.train_routes import train_bp
+from routes.booking_routes import booking_bp
+from routes.admin_routes import admin_bp
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+jwt = JWTManager(app)
+
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(train_bp, url_prefix="/train")
+app.register_blueprint(booking_bp, url_prefix="/booking")
+app.register_blueprint(admin_bp, url_prefix="/admin")
+
+if __name__ == "__main__":
+    app.run(debug=True)
