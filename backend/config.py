6abbdcab73e714b782581d79f45bb37f496880d5
Shweta Dayal Singh Rawat <shwetabts306@gmail.com>
2026-06-22 17:33:09 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,6 @@
-
+class Config:
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "Reset@123"
+    MYSQL_DB = "railway_management"
+    JWT_SECRET_KEY = "railway-secret-key"
